2350672edb5d525b2df1eeeb2fb2e03c1ec65ba2
Dakota Nelson <Dakota.Nelson@students.olin.edu>
2016-10-30 20:49:03 -0400

    new method to blur data

--- blur.py
+++ blur.py
@@ -1,9 +1,35 @@
+import random
+
 from PIL import Image, ImageFilter
 import numpy as np
 
-def blur(img, blur_radius = 0.1):
+def blur(img, blur_radius=0.2):
     img = Image.fromarray(img)
     blurred = img.copy().filter(ImageFilter.GaussianBlur(radius=blur_radius))
     blurred = np.asarray(blurred)
 
     return blurred
+
+def randomize(img, noise_level=.03):
+    """ given an array, randomizes the values in that array
+
+        noise_level [0,1] controls the overall likelihood of a bit being
+        flipped. This overall level is then multiplied by the levels variable,
+        which modifies the noise level for the various significant bit values
+        (i.e. it makes it so that less significant bits are more likely to be
+        flipped, which is accurate)
+    """
+    levels = [.005, .01, .05, .10, .15, .25, .35, .45]
+    # more or less randomly chosen modifiers for each bit significance level
+
+    for val in np.nditer(img, op_flags=['readwrite']):
+        xor_val = 0
+        for level in levels:
+            if random.random() < level * noise_level:
+                xor_val = (xor_val << 1) | 1
+            else:
+                xor_val = (xor_val << 1) | 0
+        #print('{:08b}'.format(int(xor_val)))
+        val[...] = val ^ xor_val
+
+    return img
--- demo.py
+++ demo.py
@@ -1,13 +1,18 @@
 from PIL import Image
 import numpy as np
 
-from blur import blur
+from blur import blur, randomize
 from hamming import str_to_bin, bin_to_str, encode, decode, syndrome, correct
 from stego import insert, extract
 
 # encode our original message using hamming(8,4)
-original_message = str_to_bin("we're going to hide this message in an image!")
-encoded_message = encode(original_message)
+original_message = "we're going to hide this message in an image!"
+original_message_bin = str_to_bin(original_message)
+
+print("Input:")
+print(original_message)
+
+encoded_message = encode(original_message_bin)
 
 # open up the image we want to use and get it ready to put data in
 img = Image.open("image.bmp")
@@ -16,16 +21,37 @@ img = np.asarray(img)
 # insert our message into the image, then blur the image to simulate
 # compression/transmission/general data loss
 img_with_message = insert(img, encoded_message)
-print(encoded_message)
-blurred_img = img_with_message
-#blurred_img = blur(img_with_message)
+
+im = Image.fromarray(img_with_message)
+im.save("image_steg.bmp")
+
+blur_the_image = True # change this to not blur the image
+if blur_the_image:
+    blurred_img = randomize(img_with_message)
+else:
+    blurred_img = img_with_message
+
+im = Image.fromarray(blurred_img)
+im.save("image_blurred.bmp")
 
 # extract the message (with errors) from the message, find out what the errors
 # are thanks to hamming, and generate an error syndrome (basically an array
 # that says "here's where the errors are")
 extracted_msg = extract(blurred_img)
 decoded = decode(extracted_msg)
-syndrome = syndrome(decoded)
+decoded_str = bin_to_str(decoded)
+
+print("")
+print("Decoded string:")
+print(decoded_str)
+print("")
+
+syndrome = syndrome(extracted_msg)
+
+print("")
+print("Syndrome:")
+print(syndrome.T[:100])
+print("")
 
 # using the syndrome, correct the errors in the message, then decode the
 # corrected version and convert it to a string
@@ -34,4 +60,6 @@ final_msg = decode(corrected)
 final_msg_string = bin_to_str(final_msg)
 
 # and we're done!
+print("")
+print("Output:")
 print(final_msg_string)
